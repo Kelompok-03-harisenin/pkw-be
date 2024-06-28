38c2c8349e5c9e24b0392a2efabd5ccc87065722
aufaizza <aufashidqiizza@gmail.com>
2024-06-28 14:07:03 +0700

    refixed the config database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,14 @@
 const path = require("path");
-require("dotenv").config({ path: path.join(__dirname, "../../.env") });
+
+require("dotenv").config({path: path.join(__dirname, "../../.env")});
 
 module.exports = {
   development: {
     dialect: "mysql",
-    host: process.env.DATABASE_HOST || '127.0.0.1',
-    port: process.env.DATABASE_PORT || 3306,
-    username: process.env.DATABASE_USER || 'root',
-    password: process.env.DATABASE_PASSWORD || null,
-    database: process.env.DATABASE_NAME || 'database_development',
-    logging: console.log, // Enable logging
-  },
-  // other environments...
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
+  }
 };
