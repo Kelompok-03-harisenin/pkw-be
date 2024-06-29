080ba7c5c6d365065867f5b6b21efe5de612f20d
aufaizza <aufashidqiizza@gmail.com>
2024-06-29 17:52:22 +0700

    added login/registration api

--- src/index.js
+++ src/index.js
@@ -6,8 +6,13 @@ const express = require("express");
 
 const app = express();
 
+const authRouter = require("./routes/auth.route")
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use("/api/auth", authRouter)
+
 
 app.listen(process.env.SERVER_PORT || 3000, () => {
   console.log("Server Running");
