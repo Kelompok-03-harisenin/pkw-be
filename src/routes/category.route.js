e3a886d0a54904667c0c2a4afe1969c6cf5462f7
Pebrialdi <>
2024-07-05 15:15:16 +0700

    more category have been updated

--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { index, show } = require('../controllers/category.controller');
+const { index, show, search } = require('../controllers/category.controller');
 
-router.get('/categories', index);
-router.get('/categories/:id', show);
+router.get('/', index);
+router.get('/:id', show);
+router.get('/search', search); // pencarian kategori
 
 module.exports = router;
