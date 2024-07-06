02910d86e0543fcf870539c3c27ccf3e0d4f2b47
Pebrialdi <>
2024-07-06 12:11:52 +0700

    The search feature has been improved

--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -3,13 +3,8 @@ const router = express.Router();
 const { index, show } = require('../controllers/category.controller');
 const { search } = require('../controllers/categorySearch.controller');
 
-// Get all categories
 router.get('/', index);
-
-// Get category by ID
 router.get('/:id', show);
-
-// Search categories by name
 router.get('/search/:category_name', search);
 
 module.exports = router;
