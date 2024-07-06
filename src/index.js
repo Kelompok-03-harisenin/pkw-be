6a55cb4cbb76882dbb682e5999f0fe9c1ef6cd2c
JordJ19 <jordie.juwono96@gmail.com>
2024-07-06 19:22:31 +0700

    resolve conflict

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const photoRouter = require("./routes/photo_routes");
 
 require("dotenv").config({ path: path.join(__dirname, "../.env") });
 
@@ -8,16 +7,14 @@ const app = express();
 
 const authRouter = require("./routes/auth.route");
 const categoryRouter = require("./routes/category.route");
-const authRouter = require("./routes/auth.route")
-const photoRouter = require("./routes/photo.route")
+const photoRouter = require("./routes/photo.route");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
 app.use("/api/categories", categoryRouter);
-app.use("/api/auth", authRouter)
-app.use("/api/photos", photoRouter)
+app.use("/api/photos", photoRouter);
 
 
 app.listen(process.env.SERVER_PORT || 3000, () => {
--- src/routes/photo.route.js
+++ src/routes/photo.route.js
@@ -1,9 +1,12 @@
 const express = require("express")
-
+const {editPhoto, deletePhoto} = require('../controllers/photo_controllers');
+const { verifyToken } = require("../middlewares/auth");
 const router = express.Router()
 
 const {photoPreview} = require("../controllers/photo_preview.controller")
 
-router.get("/preview/:photoID", photoPreview)
+router.get("/preview/:photoID", photoPreview);
+router.post('/photo/:photoId', verifyToken, editPhoto);
+router.delete('/photo/:photoId', verifyToken, deletePhoto);
 
-module.exports = router
+module.exports = router;
