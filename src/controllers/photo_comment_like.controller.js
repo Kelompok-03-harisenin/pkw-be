c4572a0b708f6190bb35f9e48adf2a5614ca6940
aufaizza <aufashidqiizza@gmail.com>
2024-07-08 12:26:39 +0700

    added get comments, get likes, and add comments

--- src/controllers/photo_comment_like.controller.js
+++ src/controllers/photo_comment_like.controller.js
@@ -1,68 +1,126 @@
-const { Photo:PhotoModel, Comment:commentModel, Like:likeModel, User:userModel } = require("../models")
+const { Photo: PhotoModel, Comment: commentModel, Like: likeModel, User: userModel } = require("../models")
 
-const photoComment = async (req, res, next) => {
-    const photoID = req.params.photoID
-    const photoIDNum = Number(photoID)
+/**
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ */
+const photoGetComments = async (req, res, next) => {
+  const photoID = req.params.photoID
+  const photoIDNum = Number(photoID)
 
-    const photoFind = await PhotoModel.findOne({
-        where: {id: photoIDNum},
+  const photoFind = await PhotoModel.findOne({
+    where: { id: photoIDNum },
+  })
+
+  if (!photoFind) {
+    return res.status(404).send({
+      message: "Error Photo not found"
     })
+  }
 
-    if (!photoFind) {
-        return res.status(404).send({
-            message: "Error Photo not found"
-        })
-    }
-
-    const commentFind = await commentModel.findAll({
-        where: { id_photo: photoIDNum },
-        attributes: ["id_user", "comment"],
-        include: [
-            {
-                model: userModel,
-                attributes: ["id", "name"],
-                as: "user"
-            }
-        ]
-    })
+  const commentFind = await commentModel.findAll({
+    where: { id_photo: photoIDNum },
+    attributes: ["id", "id_user", "id_photo", "comment"],
+    include: [
+      {
+        model: userModel,
+        attributes: ["id", "name"],
+        as: "user"
+      }
+    ]
+  })
 
 
-    return res.status(200).send({
-        message: "Photo found",
-        data: commentFind
-    })
+  return res.status(200).send({
+    message: "Photo found",
+    data: commentFind
+  })
 }
 
-const photoLike = async (req, res, next) => {
-    const photoID = req.params.photoID
-    const photoIDNum = Number(photoID)
+/**
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ */
+const photoAddComments = async (req, res, next) => {
+  const { comment } = req.body
 
-    const photoFind = await PhotoModel.findOne({
-        where: {id: photoIDNum},
+  if (!comment) {
+    return res.status(401).send({ message: "ERROR body is empty/isnt filled properly" })
+  }
+
+  const photoID = req.params.photoID
+  const photoIDNum = Number(photoID)
+
+  const photoFind = await PhotoModel.findOne({
+    where: { id: photoIDNum },
+  })
+
+  if (!photoFind) {
+    return res.status(404).send({
+      message: "Error Photo not found"
     })
+  }
 
-    if (!photoFind) {
-        return res.status(404).send({
-            message: "Error Photo not found"
-        })
+  const user = req.user
+
+  const commentCreate = await commentModel.create({
+    id_photo: photoIDNum,
+    id_user: user.id,
+    comment: comment
+  })
+
+  if (!commentCreate) {
+    return res.status(401).send({ message: "Comment creation failed" })
+  }
+
+  return res.status(201).send({
+    message: "Comment creation successful",
+    data: {
+      id: commentCreate.id,
+      id_photo: commentCreate.id_photo,
+      id_user: commentCreate.id_user,
+      comment: commentCreate.comment
     }
-
-    const likeFind = await likeModel.findAll({
-        where: { id_photo: photoIDNum },
-        attributes: ["id_user"],
-        include: [
-            {
-                model: userModel,
-                attributes: ["id", "name"],
-                as: "user"
-            }
-        ]
-    })
-
-    return res.status(200).send({
-        message: "Photo found",
-        data: likeFind
-    })
+  })
 }
 
-module.exports = { photoComment, photoLike }
+/**
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ */
+const photoGetLikes = async (req, res, next) => {
+  const photoID = req.params.photoID
+  const photoIDNum = Number(photoID)
+
+  const photoFind = await PhotoModel.findOne({
+    where: { id: photoIDNum },
+  })
+
+  if (!photoFind) {
+    return res.status(404).send({
+      message: "Error Photo not found"
+    })
+  }
+
+  const likeFind = await likeModel.findAll({
+    where: { id_photo: photoIDNum },
+    attributes: ["id", "id_user"],
+    include: [
+      {
+        model: userModel,
+        attributes: ["id", "name"],
+        as: "user"
+      }
+    ]
+  })
+
+  return res.status(200).send({
+    message: "Photo found",
+    data: likeFind
+  })
+}
+
+module.exports = { photoGetComments, photoGetLikes, photoAddComments }
--- src/routes/photo.route.js
+++ src/routes/photo.route.js
@@ -2,15 +2,16 @@ const express = require("express")
 
 const router = express.Router()
 
-const {photoPreview} = require("../controllers/photo_preview.controller")
-const { photoLike, photoComment } = require("../controllers/photo_comment_like.controller")
-const {editPhoto, deletePhoto} = require('../controllers/photo_controllers');
+const { photoPreview } = require("../controllers/photo_preview.controller")
+const { photoGetLikes, photoGetComments, photoAddComments } = require("../controllers/photo_comment_like.controller");
+const { editPhoto, deletePhoto } = require('../controllers/photo_controllers');
 const { verifyToken } = require("../middlewares/auth");
 
-router.get("/:photoID", photoPreview)
+router.get("/:photoID", photoPreview);
 router.post('/:photoId', verifyToken, editPhoto);
 router.delete('/:photoId', verifyToken, deletePhoto);
-router.get('/:photoID/comments', photoComment );
-router.get('/:photoID/likes', photoLike )
+router.post('/:photoID/comments', verifyToken, photoAddComments);
+router.get('/:photoID/comments', photoGetComments);
+router.get('/:photoID/likes', photoGetLikes);
 
 module.exports = router
