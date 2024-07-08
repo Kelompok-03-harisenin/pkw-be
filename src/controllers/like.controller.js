3709335531726af01f1ffd92c007d0e81642ebd2
aufaizza <aufashidqiizza@gmail.com>
2024-07-08 20:02:31 +0700

    added add likes and remove likes

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -5,7 +5,7 @@ const { Photo: PhotoModel, Like: likeModel, User: userModel } = require("../mode
  * @param {import("express").Response} res
  * @param {import("express").NextFunction} next
  */
-const getLikesByID = async (req, res, next) => {
+const getLikesByPhotoID = async (req, res, next) => {
   const photoID = req.params.photoID
   const photoIDNum = Number(photoID)
 
@@ -37,4 +37,108 @@ const getLikesByID = async (req, res, next) => {
   })
 }
 
-module.exports = { getLikesByID }
+/**
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ */
+const addLikeByPhotoID = async (req, res, next) => {
+  const photoID = req.params.photoID
+  const photoIDNum = Number(photoID)
+
+  const photoFind = await PhotoModel.findOne({
+    where: { id: photoIDNum },
+  })
+
+  if (!photoFind) {
+    return res.status(404).send({
+      message: "Error Photo not found"
+    })
+  }
+
+  const user = req.user
+
+  const likeFind = await likeModel.findOne({
+    where: {
+      id_photo: photoIDNum,
+      id_user: user.id
+    }
+  })
+
+  if (likeFind) {
+    return res.status(409).send({ message: "Error, Like already exists" })
+  }
+
+  const likeCreate = await likeModel.create({
+    id_photo: photoIDNum,
+    id_user: user.id
+  })
+
+  if (!likeCreate) {
+    return res.status(500).send({ message: "Like creation failed" })
+  }
+
+  return res.status(201).send({
+    message: "Like creation successful",
+    data: {
+      id: likeCreate.id,
+      id_photo: likeCreate.id_photo,
+      id_user: likeCreate.id_user
+    }
+  })
+}
+
+
+/**
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ */
+const removeLikeByPhotoID = async (req, res, next) => {
+  const photoID = req.params.photoID
+  const photoIDNum = Number(photoID)
+
+  const photoFind = await PhotoModel.findOne({
+    where: { id: photoIDNum },
+  })
+
+  if (!photoFind) {
+    return res.status(404).send({
+      message: "Error Photo not found"
+    })
+  }
+
+  const user = req.user
+
+  const likeFind = await likeModel.findOne({
+    where: {
+      id_photo: photoIDNum,
+      id_user: user.id
+    }
+  })
+
+  if (!likeFind) {
+    return res.status(404).send({ message: "Error, Like doesnt exist" })
+  }
+
+  const deletedLike = await likeModel.destroy({
+    where: {
+      id_photo: photoIDNum,
+      id_user: user.id
+    }
+  })
+
+  if (!deletedLike) {
+    return res.status(500).send({ message: "Error unable to delete comment" })
+  }
+
+  return res.status(200).send({
+    message: "Comment removed",
+    data: {
+      id_photo: likeFind.id_photo,
+      id_user: likeFind.id_user
+    }
+  })
+}
+
+module.exports = { getLikesByPhotoID, addLikeByPhotoID, removeLikeByPhotoID }
--- src/routes/like.route.js
+++ src/routes/like.route.js
@@ -3,8 +3,10 @@ const express = require("express");
 const router = express.Router();
 
 const { verifyToken } = require("../middlewares/auth");
-const { getLikesByID } = require("../controllers/like.controller")
+const { getLikesByPhotoID, addLikeByPhotoID, removeLikeByPhotoID } = require("../controllers/like.controller")
 
-router.get("/photo/:photoID", getLikesByID)
+router.get("/photo/:photoID", getLikesByPhotoID)
+router.post("/photo/:photoID", verifyToken, addLikeByPhotoID)
+router.delete("/photo/:photoID", verifyToken, removeLikeByPhotoID)
 
 module.exports = router;
