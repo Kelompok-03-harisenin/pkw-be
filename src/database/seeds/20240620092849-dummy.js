4ffee0fad1e2b3d40d68be77ec7437aa2bf391f1
aufaizza <aufashidqiizza@gmail.com>
2024-07-10 18:22:32 +0700

    added more dummy seeds

--- src/database/seeds/20240620092849-dummy.js
+++ src/database/seeds/20240620092849-dummy.js
@@ -61,24 +61,52 @@ module.exports = {
     await queryInterface.bulkInsert('Photos', [
       {
         id_user: 1,
-        id_category: 1,
-        photo_url: 'http://example.com/photo1.jpg',
-        title: 'Nature Photo 1',
-        description: 'A beautiful nature scene.'
-      },
-      {
-        id_user: 1,
-        id_category: 2,
-        photo_url: 'http://example.com/photo2.jpg',
-        title: 'Tech Photo 1',
-        description: 'A stunning piece of technology.'
+        id_category: 6,
+        photo_url: 'https://res.cloudinary.com/de3qj7pyl/image/upload/v1718540288/cld-sample.jpg',
+        title: 'Doggy',
+        description: 'Cute dog'
       },
       {
         id_user: 2,
         id_category: 1,
-        photo_url: 'http://example.com/photo3.jpg',
-        title: 'Nature Photo 2',
-        description: 'Another amazing nature scene.'
+        photo_url: 'https://res.cloudinary.com/de3qj7pyl/image/upload/v1718540255/sample.jpg',
+        title: 'Flowers',
+        description: 'A Beautiful flowers'
+      },
+      {
+        id_user: 3,
+        id_category: 2,
+        photo_url: 'https://res.cloudinary.com/de3qj7pyl/image/upload/v1718540281/samples/balloons.jpg',
+        title: 'Balloons',
+        description: 'Amazing wallpapers'
+      },
+      {
+        id_user: 1,
+        id_category: 4,
+        photo_url: 'https://res.cloudinary.com/de3qj7pyl/image/upload/v1718540284/samples/man-on-a-street.jpg',
+        title: 'Man on bench',
+        description: 'Bench sitter'
+      },
+      {
+        id_user: 2,
+        id_category: 3,
+        photo_url: 'https://res.cloudinary.com/de3qj7pyl/image/upload/v1718540261/samples/ecommerce/shoes.png',
+        title: 'Shoes',
+        description: 'Beautiful shoes'
+      },
+      {
+        id_user: 3,
+        id_category: 5,
+        photo_url: 'https://res.cloudinary.com/de3qj7pyl/image/upload/v1718540289/cld-sample-2.jpg',
+        title: 'Mountains',
+        description: 'Beautiful scenery'
+      },
+      {
+        id_user: 1,
+        id_category: 7,
+        photo_url: 'https://res.cloudinary.com/de3qj7pyl/image/upload/v1718540257/samples/animals/cat.jpg',
+        title: 'Cat',
+        description: 'Meow'
       }
     ]);
 
@@ -93,7 +121,27 @@ module.exports = {
         id_photo: 2,
         id_user: 2,
         comment: 'Amazing technology!'
-      }
+      },
+      {
+        id_photo: 3,
+        id_user: 3,
+        comment: 'Beautiful photo!'
+      },
+      {
+        id_photo: 4,
+        id_user: 1,
+        comment: 'Amazing technology!'
+      },
+      {
+        id_photo: 5,
+        id_user: 2,
+        comment: 'Beautiful photo!'
+      },
+      {
+        id_photo: 6,
+        id_user: 3,
+        comment: 'Amazing technology!'
+      },
     ]);
 
     // Seed Likes
@@ -105,7 +153,23 @@ module.exports = {
       {
         id_photo: 2,
         id_user: 1,
-      }
+      },
+      {
+        id_photo: 3,
+        id_user: 1
+      },
+      {
+        id_photo: 4,
+        id_user: 1,
+      },
+      {
+        id_photo: 1,
+        id_user: 2
+      },
+      {
+        id_photo: 1,
+        id_user: 3,
+      },
     ]);
 
   },
